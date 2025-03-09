991cce95105a036916e0d5999dd165e432217383
DamareonC <markeec2002@gmail.com>
2025-03-09 00:15:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# NEdit
+
+Text editing software made with Java and Java Swing.
+
+## Prerequisites
+
+* Java 21
+
+## Build
+
+To build NEdit, run: `./mvnw package` (Mac/Linux) or `mvnw.cmd package`
+
+## Run
+
+To run NEdit, run: `java -jar ./target/NEdit-1.0-SNAPSHOT.jar`
---
+++ src/main/java/app/damareonc/nedit/Main.java
@@ -0,0 +1,15 @@
+package app.damareonc.nedit;
+
+import javax.swing.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame jFrame = new JFrame("NEdit");
+
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(640, 480);
+        jFrame.setVisible(true);
+    }
+}
